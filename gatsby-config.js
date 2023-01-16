480c9de428b6e065e79e98673deb0ab3ff6b3317
Vinoth0109 <118457180+Vinoth0109@users.noreply.github.com>
2023-01-16 12:44:30 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GATSBY_LAB`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
